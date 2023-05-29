5696fbdd4832c76cfe2d54cfdc4972cffc34bcc2
Theo602 <110599214+Theo602@users.noreply.github.com>
2023-05-29 15:42:04 +0200

    connexion de l'application aux deveTools

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,6 +1,9 @@
 import { combineReducers, createStore } from "redux";
 import { themeReducer } from "../features/theme"
 
+// Pour connecter les Redux Devtools on utilise une fonction disponible sur l'objet window
+// Si cette fonction existe on l'exécute.
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 // on utilise combineReducer pour faire fonctionner plusieurs reducers ensemble
 const reducer = combineReducers({
@@ -11,4 +14,4 @@ const reducer = combineReducers({
 // pour initialiser le store
 // Pas besoin de passer de state initial
 // car chaque reducer à son propre state initial
-export const store = createStore(reducer);
+export const store = createStore(reducer, reduxDevtools);
